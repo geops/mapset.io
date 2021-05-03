07e56f2fa1d523b915b8e51e0677211d6a10acf2
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2021-05-03 13:24:56 +0200

    Remove unused stuff.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,1 @@
 import CMS from "netlify-cms-app";
-
-import uploadcare from "netlify-cms-media-library-uploadcare";
-import cloudinary from "netlify-cms-media-library-cloudinary";
-
-// import IndexPagePreview from './preview-templates/IndexPagePreview'
-
-CMS.registerMediaLibrary(uploadcare);
-CMS.registerMediaLibrary(cloudinary);
-
-// CMS.registerPreviewTemplate('index', IndexPagePreview)
-
-// TODO register/style content and benefits previews
