b20bd73a3d5176c92361201b52f074450c64fd2c
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2021-05-25 15:55:14 +0200

    Fix path.

--- src/pages/index.js
+++ src/pages/index.js
@@ -463,7 +463,7 @@ export const IndexPageTemplate = ({ locale, region }) => {
         type="text/javascript"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "[https://snap.licdn.com/li.lms-analytics/insight.min.js]"; s.parentNode.insertBefore(b, s);})();`,
+            (function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})();`,
         }}
       />
       <noscript>
