3093b5ff05c8c48dd80a7033d16cac0422b22604
danji90 <daniel.marsh-hunn@geops.com>
2022-10-11 12:44:32 +0200

    chore: clean

--- src/utils/getUrl.js
+++ src/utils/getUrl.js
@@ -2,18 +2,13 @@ export const getLocalizedUrl = (region, locale, url, reset) => {
   if (!url && typeof window === `undefined`) {
     return;
   }
-
-  if (!!region) console.log(reset);
   const validatedUrl = url || window.location.href;
   const urlObject = new URL(validatedUrl);
   const { pathname } = urlObject;
   if (region && locale) {
-    console.log(url, reset);
     if (region === 'ch') {
       urlObject.pathname = `/${locale !== 'de' ? `${locale}/` : ''}${!reset ? '' : pathname.split('/').slice(-1).pop()}`;
     } else {
-      // console.log('path: ', pathname.split('/').slice(-1).pop(), reset);
-      // console.log(url, `/${locale !== 'en' ? `${locale}/` : ''}${reset ? '' : pathname.split('/').slice(-1).pop()}`);
       urlObject.pathname = `/${locale !== 'en' ? `${locale}/` : ''}${reset ? '' : pathname.split('/').slice(-1).pop()}`;
     }
   }
