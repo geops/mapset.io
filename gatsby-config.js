00b9e43721508341954504eef40214a97a659b7c
danji_ma90 <daniel.marsh-hunn@geops.com>
2023-07-04 09:09:44 +0200

    fix(login): remove IdentityWidget from gatsby netlify cms plugin (#105)
    * fix(login): remove IdentityWidget from gatsby netlify cms plugin
    
    * chore: downgrade plugin
    
    * chore: add NODE_OPTIONS to script
    
    * chore: set node >= 18 in package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
-        enableIdentityWidget: true,
+        enableIdentityWidget: false,
       },
     },
     {
