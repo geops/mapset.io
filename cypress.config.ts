6c4a47a3ca96d0fce715ca4e78c6d1167bac8ffb
Olivier Terral <olivier.terral@geops.com>
2023-08-03 11:49:10 +0200

    Add cypress files and github actions

---
+++ cypress.config.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  requestTimeout: 20000,
+  defaultCommandTimeout: 20000,
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 3,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 0,
+  },
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    numTestsKeptInMemory: 0,
+    chromeWebSecurity: false,
+    video: false,
+
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    // setupNodeEvents(on, config) {
+    //   // eslint-disable-next-line global-require
+    //   return require("./cypress/plugins/index.js")(on, config);
+    // },
+  },
+});
