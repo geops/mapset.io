955d21936a5b68867e21354c31f4504fbd3adf86
Olivier Terral <olivier.terral@geops.com>
2023-08-04 14:46:45 +0200

    Use plausible

---
+++ src/lib/useAnalytics.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+const domain = process.env.GATSBY_REGION;
+
+export default function useAnalytics() {
+  return useEffect(() => {
+    // We only want to track analytics in production.
+    if (process.env.NODE_ENV !== 'production') {
+      return () => {};
+    }
+
+    const plausibleSrc = 'https://plausible.geops.io/js/script.js';
+    if (!document.querySelector(`script[src="${plusibleSrc}"]`)) {
+      // <script defer data-domain="dev.mapset.io" src="https://plausible.geops.io/js/script.js"></script>
+      const firstScript = document.getElementsByTagName('script')[0];
+      const plausibleScript = document.createElement('script');
+      plausibleScript.type = 'text/javascript';
+      plausibleScript.defer = true;
+      plausibleScript.dataset.domain = 'mapset.' + domain;
+      plausibleScript.src = plausibleSrc;
+      firstScript.parentNode.insertBefore(plausibleScript, firstScript);
+    }
+
+    return () => {};
+  }, [pathname]);
+}
