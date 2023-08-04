efb4249d61feb77dda1549795540fa68616c2a0e
Olivier Terral <olivier.terral@geops.com>
2023-08-04 15:33:33 +0200

    Use same layout for guide and imprint

--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -16,6 +16,7 @@ import DotIcon from "./images/DotIcon";
 import GuideH4 from "./GuideH4";
 import { onClickSmoothScroll } from "./NavLinks";
 import SquareIcon from "./images/SquareIcon";
+import H1 from "./ui/H1";
 
 const renderId = (label: string) => {
   if (label) {
@@ -100,153 +101,150 @@ export const Guide = () => {
 
   return (
     <>
-      <div className="relative z-0 ">
-        {/* <div className="justify-center px-4 hidden md:flex">
+      <div className="relative z-0">
+        <header className="justify-center px-4 hidden md:flex">
           <Header className="container lg  text-gray py-1 justify-end"></Header>
-        </div> */}
-        <header className="bg-blue-600 text-white  z-[1000] fixed w-full t-0 px-12 py-2 flex items-center">
-          <div className="container  flex justify-between items-center">
-            <Link href={"/" + locale}>
-              <MapsetLogo />
-            </Link>
-            <Header
-              className="text-white"
-              linkClassName="hover:text-slate-300"
-              selectedClassName="text-slate-300"
-            ></Header>
-          </div>
         </header>
-        {/* <div className="flex flex-col items-center relative px-4 pt-12 bg-gradient-to-r from-blue-600 to-blue-light text-white z-10">
-          <div className="container lg">
-            <div className="flex justify-between mb-12">
-              <div></div>
-              <Link href={"/" + language}>
-                <MapsetLogo />
-              </Link>
-            </div>
-            <div className="flex justify-between overflow-hidden flex-wrap md:flex-nowrap">
-              <div className="flex flex-col gap-6 pb-12">
-                <H1>{t("guide.title")}</H1>
-                <p className="max-w-[646px] text-2xl">{t("guide.subtitle")}</p>
+        <main>
+          <div className="flex flex-col items-center relative px-4 pt-12 bg-gradient-to-r from-blue-600 to-blue-light text-white z-10">
+            <div className="container lg">
+              <div className="flex justify-between mb-12">
+                <div></div>
+                <Link href={"/" + language}>
+                  <MapsetLogo />
+                </Link>
+              </div>
+              <div className="flex justify-between overflow-hidden flex-wrap md:flex-nowrap">
+                <div className="flex flex-col gap-6 pb-12">
+                  <H1>{t("guide.title")}</H1>
+                  {/* <p className="max-w-[646px] text-2xl">
+                    {t("guide.subtitle")}
+                  </p> */}
+                </div>
               </div>
             </div>
           </div>
-        </div> */}
-        <nav className="fixed guide-scroller w-[220px] overflow-y-auto h-full pl-4 mt-[82px] pb-[174px] ">
-          {titles.map((feature) => {
-            return (
-              <>
-                <Link
-                  href={`#${renderId(feature.label)}`}
-                  id={renderScrollerId(feature.label)}
-                  title={feature.label}
-                  className="flex items-center gap-2 py-2 hover:text-blue-600 scroll-mt-[90px]"
-                  onClick={onClickSmoothScroll}
-                >
-                  <DotIcon />
-                  <span className="text-ellipsis overflow-hidden pointer-events-none">
-                    {feature.label}
-                  </span>
-                </Link>
-                <div className="guide-scroller-sub">
-                  {(feature.sub_features || []).map((feat) => (
+          <div className="flex">
+            <nav
+              style={{
+                height: "calc(100vh - 0px)",
+              }}
+              className="guide-scroller w-[220px] max-w-[220px] sticky top-0 bottom-0 overflow-y-auto pl-4 pb-[174px]"
+            >
+              {titles.map((feature) => {
+                return (
+                  <>
                     <Link
-                      href={`#${renderId(feat)}`}
-                      id={renderScrollerId(feat)}
-                      key={renderScrollerId(feat)}
-                      title={feat}
-                      className="flex items-center gap-2 py-2 pl-4 hover:text-blue-600 scroll-mt-[90px]"
+                      href={`#${renderId(feature.label)}`}
+                      id={renderScrollerId(feature.label)}
+                      title={feature.label}
+                      className="flex items-center gap-2 py-2 hover:text-blue-600 scroll-mt-[90px]"
                       onClick={onClickSmoothScroll}
                     >
-                      <SquareIcon />
+                      <DotIcon />
                       <span className="text-ellipsis overflow-hidden pointer-events-none">
-                        {feat}
+                        {feature.label}
                       </span>
                     </Link>
-                  ))}
-                </div>
-              </>
-            );
-          })}
-        </nav>
-        <main className="pl-[220px] pt-[82px] container mx-auto max-w-[1440px]">
-          <div className="flex flex-col gap-4 pl-6 py-24 md:pl-12 lg:pl-24">
-            <H2 className="is-bolder guideHeader">{t("guide.title")}</H2>
-            <p>
-              {t("guide.links_ch")}
-              <br />
-              <Link
-                href="https://editor.mapset.ch/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline text-blue-600 hover:text-blue-900"
-              >
-                https://editor.mapset.ch/
-              </Link>
-            </p>
-            <p className="mb-12">
-              {t("guide.links_io")}
-              <br />
-              <Link
-                href="https://editor.mapset.io/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline text-blue-600 hover:text-blue-900"
-              >
-                https://editor.mapset.io/
-              </Link>
-            </p>
-            {(guideContent || []).map((topic) => {
-              const text = md.render(topic.label);
-              return (
-                <div
-                  id={renderId(topic.label)}
-                  key={renderId(topic.label)}
-                  className="scroll-mt-[90px]"
+                    <div className="guide-scroller-sub">
+                      {(feature.sub_features || []).map((feat) => (
+                        <Link
+                          href={`#${renderId(feat)}`}
+                          id={renderScrollerId(feat)}
+                          key={renderScrollerId(feat)}
+                          title={feat}
+                          className="flex items-center gap-2 py-2 pl-4 hover:text-blue-600 scroll-mt-[90px]"
+                          onClick={onClickSmoothScroll}
+                        >
+                          <SquareIcon />
+                          <span className="text-ellipsis overflow-hidden pointer-events-none">
+                            {feat}
+                          </span>
+                        </Link>
+                      ))}
+                    </div>
+                  </>
+                );
+              })}
+            </nav>
+            <div className="pl-[220px] container mx-auto max-w-[1440px]  flex flex-col gap-4 py-24 md:pl-12 lg:pl-24">
+              {/* <H2 className="is-bolder guideHeader">{t("guide.title")}</H2> */}
+              <p>
+                {t("guide.links_ch")}
+                <br />
+                <Link
+                  href="https://editor.mapset.ch/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="underline text-blue-600 hover:text-blue-900"
                 >
-                  <GuideH4 icon={topic.mapset_icon} text={text} />
+                  https://editor.mapset.ch/
+                </Link>
+              </p>
+              <p className="mb-12">
+                {t("guide.links_io")}
+                <br />
+                <Link
+                  href="https://editor.mapset.io/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="underline text-blue-600 hover:text-blue-900"
+                >
+                  https://editor.mapset.io/
+                </Link>
+              </p>
+              {(guideContent || []).map((topic) => {
+                const text = md.render(topic.label);
+                return (
+                  <div
+                    id={renderId(topic.label)}
+                    key={renderId(topic.label)}
+                    className="scroll-mt-[90px]"
+                  >
+                    <GuideH4 icon={topic.mapset_icon} text={text} />
 
-                  {topic.content.map((f) => {
-                    const text = md.render(f.heading);
+                    {topic.content.map((f) => {
+                      const text = md.render(f.heading);
 
-                    return (
-                      <div
-                        key={renderId(f.heading)}
-                        id={renderId(f.heading)}
-                        className="px-12 py-6 scroll-mt-[90px]"
-                      >
-                        <GuideH4 icon={f.mapset_icon} text={text} />
-
-                        {!!f.login_restricted && (
-                          <p className="flex gap-2 text-blue-600">
-                            <WarningIcon />
-                            <i>{t("guide.login_restricted")}</i>
-                          </p>
-                        )}
-
-                        {!!f.cd_restricted && (
-                          <p className="flex gap-2 text-blue-600">
-                            <WarningIcon />
-                            <i>{t("guide.cd_restricted")}</i>
-                          </p>
-                        )}
+                      return (
                         <div
-                          // className="pl-12"
-                          dangerouslySetInnerHTML={{
-                            __html: md.render(f.text),
-                          }}
-                        />
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
+                          key={renderId(f.heading)}
+                          id={renderId(f.heading)}
+                          className="px-12 py-6 scroll-mt-[90px]"
+                        >
+                          <GuideH4 icon={f.mapset_icon} text={text} />
 
-            <H2 className="is-bolder guideHeader">
-              {t("guide.more_questions")}
-            </H2>
-            <Contact region={domain} />
+                          {!!f.login_restricted && (
+                            <p className="flex gap-2 text-blue-600">
+                              <WarningIcon />
+                              <i>{t("guide.login_restricted")}</i>
+                            </p>
+                          )}
+
+                          {!!f.cd_restricted && (
+                            <p className="flex gap-2 text-blue-600">
+                              <WarningIcon />
+                              <i>{t("guide.cd_restricted")}</i>
+                            </p>
+                          )}
+                          <div
+                            // className="pl-12"
+                            dangerouslySetInnerHTML={{
+                              __html: md.render(f.text),
+                            }}
+                          />
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+
+              <H2 className="is-bolder guideHeader">
+                {t("guide.more_questions")}
+              </H2>
+              <Contact region={domain} />
+            </div>
           </div>
         </main>
         <Footer className="z-[1000] relative" />
