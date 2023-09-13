a9090619a18f3b89ac0f8b5f832d4a9262d4f867
danji90 <daniel.marsh-hunn@geops.com>
2023-09-13 10:23:13 +0200

    fix(clients): add SZU logo

--- src/utils/clientLogos.js
+++ src/utils/clientLogos.js
@@ -1,15 +1,16 @@
-import sbb_logo from "../img/clients/sbb_logo.png";
 import bogg_logo from "../img/clients/bogg_logo.png";
 import cheminsDeFerJura_logo from "../img/clients/cheminsdefer_jura_logo.png";
 import churbus_logo from "../img/clients/churbus_logo.png";
 import engadin_logo from "../img/clients/engadinbus_logo.png";
 import forchbahn_logo from "../img/clients/forchbahn_logo.png";
 import gwb_logo from "../img/clients/gwb_logo.png";
-import sob_logo from "../img/clients/sob_logo.png";
 import rbs_logo from "../img/clients/rbs_logo.png";
+import sbb_logo from "../img/clients/sbb_logo.png";
+import sob_logo from "../img/clients/sob_logo.png";
+import win_logo from "../img/clients/stadtbus_winterthur_logo.png";
+import szu_logo from "../img/clients/szu_logo.png";
 import sti_logo from "../img/clients/sti_logo.png";
 import tmr_logo from "../img/clients/tmr_logo.png";
-import win_logo from "../img/clients/stadtbus_winterthur_logo.png";
 import trenord_logo from "../img/clients/trenord_logo.png";
 import vbg_logo from "../img/clients/vbg_logo.png";
 import vbz_logo from "../img/clients/vbz_logo.png";
@@ -59,6 +60,10 @@ const clientLogos = [
   {
     id: "sti",
     src: sti_logo,
+  },
+  {
+    id: "szu",
+    src: szu_logo,
   },
   {
     id: "tmr",
